ab7a1283fd1d0cc90e22c9c91b8c73be9fff213f
Madina Amankeldinova <madina.amankeldinova@nu.edu.kz>
2023-05-29 22:37:36 +0600

    added button play

--- Netflix/Controllers/HomeViewController.swift
+++ Netflix/Controllers/HomeViewController.swift
@@ -10,10 +10,14 @@ import UIKit
 class HomeViewController: UIViewController {
     
     
+    let sectionTitles: [String] = ["Trending Movies","Popular","Trending Tv", "Upcoming Movies", "Top rated"]
+    
+    
     private let homeFeedTable: UITableView = {
         
         let table = UITableView(frame: .zero, style: .grouped)
-        table.register(CollestionTableViewCell.self, forCellReuseIdentifier: CollestionTableViewCell.identifier)
+        table.register(CollestionTableViewCell.self,
+        forCellReuseIdentifier: CollestionTableViewCell.identifier)
         return table
     }()
     
@@ -27,7 +31,22 @@ class HomeViewController: UIViewController {
         homeFeedTable.delegate = self
         homeFeedTable.dataSource = self
         
-        homeFeedTable.tableHeaderView = UIView(frame:  CGRect(x: 0, y: 0, width: view.bounds.width, height: 45))
+        configureNavBar()
+        
+        let headerView = HeroHeaderUIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 450))
+        homeFeedTable.tableHeaderView = headerView
+    }
+    
+    private func configureNavBar() {
+        var image = UIImage(named: "netflixLogo")
+        image = image?.withRenderingMode(.alwaysOriginal)
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: image, style: .done, target: self, action: nil)
+        
+        navigationItem.rightBarButtonItems = [
+            UIBarButtonItem(image: UIImage(systemName: "person"), style: .done, target: self, action: nil),
+            UIBarButtonItem(image: UIImage(systemName: "play.rectangle"), style: .done, target: self, action: nil)
+        ]
+        navigationController?.navigationBar.tintColor = .white
     }
     
     
@@ -41,7 +60,7 @@ class HomeViewController: UIViewController {
 
 extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 20
+        return sectionTitles.count
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -63,5 +82,24 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
         return 40
     }
     
+    func tableView(_ tableView: UITableView, willDisplayHeaderView  view: UIView, forSection section: Int) {
+        guard let header = view as? UITableViewHeaderFooterView else {return}
+        header.textLabel?.font = .systemFont(ofSize: 18, weight: .semibold)
+        header.textLabel?.frame = CGRect(x: header.bounds.origin.x + 20, y: header.bounds.origin.y, width: 100, height: header.bounds.height)
+        header.textLabel?.textColor = .white
+        header.textLabel?.text = header.textLabel?.text?.lowercased()
+
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection  section: Int) -> String? {
+        return sectionTitles[section]
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let defaultOffSet = view.safeAreaInsets.top
+        let offset = scrollView.contentOffset.y + defaultOffSet
+        
+        navigationController?.navigationBar.transform = .init(translationX: 0, y: min(0, -offset))
+    }
     
 }
---
+++ Netflix/Managers/APICaller.swift
@@ -0,0 +1,22 @@
+//
+//  APICaller.swift
+//  Netflix
+//
+//  Created by Madina Amankeldinova on 29.05.2023.
+//
+
+import Foundation
+
+struct Constants {
+    static let API_KEY = "522ff245d84b4210f4853b1deeb56bc7"
+}
+
+
+class APICaller {
+    static let shared = APICaller()
+    
+    
+    func getTrendingMovies(completion: @escaping (String)-> Void) {
+        
+    }
+}
