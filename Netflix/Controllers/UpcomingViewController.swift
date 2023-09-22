efebe9164bbb4107942a862871571abf291ae8df
Madina Amankeldinova <madina.amankeldinova@nu.edu.kz>
2023-09-22 14:13:25 +0600

    Upcoming movies titles added

--- Netflix/Controllers/UpcomingViewController.swift
+++ Netflix/Controllers/UpcomingViewController.swift
@@ -9,16 +9,18 @@ import UIKit
 
 class UpcomingViewController: UIViewController {
     
+    private var titles: [Title] = [Title]()
     
     private let upcomingTable: UITableView = {
-        
+
+
         let table = UITableView()
         table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
         return table
     }()
     
     override func viewDidLoad() {
-            super.viewDidLoad()
+        super.viewDidLoad()
         
         view.backgroundColor = .systemBackground
         title = "Upcoming"
@@ -29,13 +31,43 @@ class UpcomingViewController: UIViewController {
         view.addSubview(upcomingTable)
         upcomingTable.delegate = self
         upcomingTable.dataSource = self
+        
+        fetchUpComing()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        upcomingTable.frame = view.bounds
+    }
+    
+    
+    private func fetchUpComing() {
+        APICaller.shared.getUpComingMovies { [weak self] result in
+            
+            switch result {
+            case .success(let titles):
+                self?.titles = titles
+                DispatchQueue.main.async {
+                    self?.upcomingTable.reloadData()
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+                
+            }
+        }
     }
 }
 
 
 
 extension UpcomingViewController: UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        <#code#>
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return titles.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = titles[indexPath.row].original_name ?? titles[indexPath.row].original_title ?? "Unknown"
+        return cell
+
     }
 }
